1a3d981a8f7200f46af1172cf927087ed4cb4e70
richarddempsey <richarddempsey@morrisonsplc.co.uk>
2018-04-01 05:02:30 +0100

    changed hello world message

--- src/main/java/com/rmd/learning/App.java
+++ src/main/java/com/rmd/learning/App.java
@@ -10,7 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        Stream.of("Hello", " ", "World", "/n").forEach(System.out::print);
+        Stream.of("Hello", " ", "WorldZ", "/n").forEach(System.out::print);
 
         //System.out.println( "Hello World!" );
     }
